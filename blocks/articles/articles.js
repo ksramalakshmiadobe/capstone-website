fb18dfa095af71018e42cb22c29d38efda551955
ksramalakshmiadobe <ramalakshmi@adobe.com>
2024-06-21 12:57:19 +0530

    saving changes

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -1,4 +1,5 @@
 export default async function decorate(block) {
     let data = await fetch("https://main--capstone-website--ksramalakshmiadobe.hlx.live/magazine/magazine-index.json");
-    console.log(data);
+    let response = await data.json()
+    console.log(response);
   }
